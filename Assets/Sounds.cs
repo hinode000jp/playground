8670873f19b843c2b47289424f8e31759627ba50
hinode000jp <takenoko000jp@gmail.com>
2019-07-28 18:42:13 +0900

    add sound

---
+++ Assets/Sounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    AudioSource getSE;
+
+    // Start is called before the first frame update
+    void Start()
+    {        getSE = GetComponent<AudioSource>();    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)    {        if (other.gameObject.CompareTag("Player"))        {                        getSE.Play();        }    }
+    private void OnCollisionEnter(Collision collision)    {        if (collision.gameObject.CompareTag("Player"))        {            getSE.Play();        }    }
+}
